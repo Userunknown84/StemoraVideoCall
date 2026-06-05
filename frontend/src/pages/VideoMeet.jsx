ebb6e00e38f3d4f59b349107b1142a79821af671
Userunknown84 <adityasharma96469@gmail.cm>
2026-06-05 21:32:12 +0530

    commit

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -14,8 +14,9 @@ import StopScreenShareIcon from '@mui/icons-material/StopScreenShare';
 import ChatIcon from '@mui/icons-material/Chat';
 import { Badge } from '@mui/material';
 import { useNavigate } from "react-router-dom";
+import server from '../../environment';
 
-const server_url = "http://localhost:8000";
+const server_url = server;
 
 
 var connections = {};
